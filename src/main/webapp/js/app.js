3ba04c009c03c966a4ae22737acd603a0520412f
siddharthg <siddharthg.official@gmail.com>
2017-08-21 19:51:24 +0530

    [UI-ROUTER] Create barbone dahsboard and homepage - Add support for UI router - Add place holder content panes for dahsboard - Hook-up URLs to content using ui-view - Create Links on Homepage and Dashboard to access different content

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -1,19 +1,80 @@
 var app = angular.module("navimate", [
-  'ngRoute'
+  'ui.router'
 ]);
 
 app.config(['$locationProvider', function ($locationProvider) {
-  $locationProvider.hashPrefix('');
+    $locationProvider.hashPrefix('');
 }]);
 
-app.config(function ($routeProvider) {
-  $routeProvider
-    .when('/register', {
-      templateUrl: '../views/register.html',
-      controller: 'AuthCtrl'
+app.config(function ($stateProvider, $urlRouterProvider) {
+
+    // Configure URL mapping for non existent URLs
+  $urlRouterProvider.when('/dashboard', '/dashboard/team/manage')
+  $urlRouterProvider.when('/dashboard/team', '/dashboard/team/manage')
+  $urlRouterProvider.when('/dashboard/tasks', '/dashboard/tasks/manage')
+  $urlRouterProvider.when('/dashboard/forms', '/dashboard/forms/manage')
+
+  $stateProvider
+      // Homepage Mappings
+    .state('home', {
+      abstract: true,
+      url: '',
+      templateUrl: '../views/home.html'
     })
-    .when('/login', {
-      templateUrl: '../views/login.html',
-      controller: 'AuthCtrl'
+    .state('home.features', {
+        url: '',
+        templateUrl: '../views/home/features.html'
+    })
+    .state('home.login', {
+        url: '/login',
+        templateUrl: '../views/home/login.html',
+        controller: 'AuthCtrl'
+    })
+    .state('home.register', {
+        url: '/register',
+        templateUrl: '../views/home/register.html',
+        controller: 'AuthCtrl'
+    })
+      // Dashboard mappings
+    .state('dashboard', {
+        abstract: true,
+        url: '/dashboard',
+        templateUrl: '../views/dashboard.html'
+    })
+    .state('dashboard.team', {
+        abstract: true,
+        url: '/team'
+    })
+    .state('dashboard.team.manage', {
+        url: '/manage',
+        templateUrl: '../views/dashboard/team/manage.html'
+    })
+    .state('dashboard.team.report', {
+        url: '/report',
+        templateUrl: '../views/dashboard/team/report.html'
+    })
+    .state('dashboard.tasks', {
+        abstract: true,
+        url: '/tasks'
+    })
+    .state('dashboard.tasks.manage', {
+        url: '/manage',
+        templateUrl: '../views/dashboard/tasks/manage.html'
+    })
+    .state('dashboard.tasks.report', {
+        url: '/report',
+        templateUrl: '../views/dashboard/tasks/report.html'
+    })
+    .state('dashboard.forms', {
+        abstract: true,
+        url: '/forms'
+    })
+    .state('dashboard.forms.manage', {
+        url: '/manage',
+        templateUrl: '../views/dashboard/forms/manage.html'
+    })
+    .state('dashboard.forms.edit', {
+        url: '/edit',
+        templateUrl: '../views/dashboard/forms/editor.html'
     })
 });
