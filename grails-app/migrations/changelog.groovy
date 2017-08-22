f9c0d824a921891e3592db84844ee951144a76e5
siddharthg <siddharthg.official@gmail.com>
2017-08-22 17:24:03 +0530

    [DB][SUPPORT] Create DB migration

--- grails-app/migrations/changelog.groovy
+++ grails-app/migrations/changelog.groovy
@@ -2,4 +2,5 @@ databaseChangeLog = {
     include file: 'add-account-and-user.groovy'
     include file: 'alter-last-modified-in-account-and-user.groovy'
     include file: 'add-nullable-to-account-for-user.groovy'
+    include file: 'add-biz-db.groovy'
 }
