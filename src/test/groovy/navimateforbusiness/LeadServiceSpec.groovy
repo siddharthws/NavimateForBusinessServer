417be222df58541b7bf4104505419063648a7a2f
siddharthg <siddharthg.official@gmail.com>
2017-08-28 20:29:35 +0530

    [EXCEL][BACKEND] Add support to handle excel lead upload - Add API to handle lead upload - Add service to parse leads form excel data - Serialize Lead response

---
+++ src/test/groovy/navimateforbusiness/LeadServiceSpec.groovy
@@ -0,0 +1,18 @@
+package navimateforbusiness
+
+import grails.testing.services.ServiceUnitTest
+import spock.lang.Specification
+
+class LeadServiceSpec extends Specification implements ServiceUnitTest<LeadService>{
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
