389a7e86054b41f3354be4a129d7fa5feb709c61
siddharthg <siddharthg.official@gmail.com>
2017-08-31 18:50:26 +0530

    [DIALOG][SUPPORT] Add Dialog Service which can be used to create dialogs - DialogService Exposes APIs to launch different types of dialogs - APIs accept relevant arguments

---
+++ src/main/webapp/js/services/DialogService.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Siddharth on 29-08-2017.
+ */
+
+app.service('DialogService', function($mdDialog) {
+
+    // Launch Alert Dialog
+    this.alert = function ($scope, message) {
+        // Placeholder
+    }
+
+    // Launch Confirm Dialog
+    this.confirm = function ($scope, message) {
+        // Placeholder
+    }
+})
