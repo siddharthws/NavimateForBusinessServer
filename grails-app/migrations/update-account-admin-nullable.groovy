0ffb52e39ce7b6b302a70bd852f901671ff0f89f
siddharthg <siddharthg.official@gmail.com>
2017-09-04 13:40:24 +0530

    [BUGFIX][DB] Fix Account - User Relationship - Account should have a nullable admin - admin will be assigned during registration - User shoud have a non nullable account

--- grails-app/migrations/update-account-admin-nullable.groovy
+++
@@ -1,6 +0,0 @@
-databaseChangeLog = {
-
-    changeSet(author: "Siddharth (generated)", id: "1504272985266-1") {
-        addNotNullConstraint(columnDataType: "bigint", columnName: "task_id", tableName: "form")
-    }
-}
