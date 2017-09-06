d9068f6e2315d156299221db151449195228db16
siddharthg <siddharthg.official@gmail.com>
2017-09-06 15:26:40 +0530

    [LEAD][MANAGE] Add toolbar and data for lead management - Implement backend to return a list of leads on http call - Implement striped table format to display data - Move Add Leads functionality to Lead Management toolbar

--- src/main/webapp/js/ctrls/dashboard/TaskManageCtrl.js
+++ src/main/webapp/js/ctrls/dashboard/TaskManageCtrl.js
@@ -2,22 +2,6 @@
  * Created by Siddharth on 22-08-2017.
  */
 
-app.controller("TaskManageCtrl", function ($scope, ExcelService, DialogService) {
+app.controller("TaskManageCtrl", function ($scope) {
 
-    $scope.excelRead = function (workbook) {
-        ExcelService.excelRead(workbook).then(
-            function (response) {
-                // Launch Map Editor Dialod
-                DialogService.mapEditor(response.data)
-            },
-            function (error) {
-                // Excel File Parse Error
-                console.log(error)
-            }
-        )
-    }
-
-    $scope.excelError = function (e) {
-        console.log("Excel Read Error = " + e)
-    }
 })
