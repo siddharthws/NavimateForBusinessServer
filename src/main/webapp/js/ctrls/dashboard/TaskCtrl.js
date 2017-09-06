cdfef6274cc2095a0f328f55b47fa24debf2969f
siddharthg <siddharthg.official@gmail.com>
2017-09-06 17:34:14 +0530

    [TASK][MANAGE] Populate task management content with data and toolbar - Get Task  data from backend - Show in tabular format - Add button in toolbar

--- src/main/webapp/js/ctrls/dashboard/TaskCtrl.js
+++ src/main/webapp/js/ctrls/dashboard/TaskCtrl.js
@@ -4,26 +4,4 @@
 
 app.controller("TaskCtrl", function ($scope, $http, $localStorage, $state) {
 
-    $scope.init = function ()
-    {
-        $http({
-            method:     'GET',
-            url:        '/api/users/task',
-            headers:    {
-                'X-Auth-Token':    $localStorage.accessToken
-            }
-        })
-            .then(
-                function (response) {
-                    console.log("Content View Info received")
-                },
-                function (error) {
-                    console.log(error)
-                    $state.go('home')
-                }
-            )
-    }
-
-    // Init View
-    $scope.init()
 })
