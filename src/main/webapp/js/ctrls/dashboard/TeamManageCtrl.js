41c911afe80bc152484ac5c9dc679d3e338a5492
siddharthg <siddharthg.official@gmail.com>
2017-09-07 16:31:11 +0530

    [CLEANUP][TEAM] Remove init() function

--- src/main/webapp/js/ctrls/dashboard/TeamManageCtrl.js
+++ src/main/webapp/js/ctrls/dashboard/TeamManageCtrl.js
@@ -4,26 +4,24 @@
 
 app.controller("TeamManageCtrl", function ($scope, $http, $localStorage, $state) {
 
-    $scope.init = function ()
-    {
-        $http({
-            method:     'GET',
-            url:        '/api/users/team',
-            headers:    {
-                'X-Auth-Token':    $localStorage.accessToken
-            }
-        })
-            .then(
-                function (response) {
-                    $scope.team = response.data
-                },
-                function (error) {
-                    console.log(error)
-                    $state.go('home')
-                }
-            )
-    }
+    /*--------------------------------- INIT -----------------------------------*/
+    // Get team for this user
+    $http({
+        method:     'GET',
+        url:        '/api/users/team',
+        headers:    {
+            'X-Auth-Token':    $localStorage.accessToken
+        }
+    })
+    .then(
+        function (response) {
+            $scope.team = response.data
+        },
+        function (error) {
+            console.log(error)
+            $state.go('home')
+        }
+    )
 
-    // Init View
-    $scope.init()
+    /*--------------------------------- APIs -----------------------------------*/
 })
