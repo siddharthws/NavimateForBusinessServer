82083d3994bc8aae90a85c3be6f8679091a9f64d
siddharthg <siddharthg.official@gmail.com>
2017-09-07 15:07:17 +0530

    [TASK][CREATE][DIALOG] Add task creator dialog - Dialog will help select  rep and template for each lead using dropdown - Dialog will have add button to add more tasks - Entering template and rep for each task to be created will be mandatory. Validated in frontend - Pass tasks to backend for furthr processing

--- src/main/webapp/js/ctrls/dialogs/TaskCreatorCtrl.js
+++ src/main/webapp/js/ctrls/dialogs/TaskCreatorCtrl.js
@@ -3,6 +3,112 @@
  */
 
 // Controller for Task Creator Dialog
-app.controller('TaskCreatorCtrl', function ($scope, $http, $localStorage, leads) {
-    // Placeholder
+app.controller('TaskCreatorCtrl', function ($scope, $http, $localStorage, $state, leads) {
+
+    /* ----------------------------- INIT --------------------------------*/
+    $scope.tasks = []
+    $scope.leads = []
+    $scope.team = []
+    $scope.formTemplates = []
+
+    // Create Tasks from Leads (empty rep and template)
+    leads.forEach(function (lead) {
+        var task = {
+            lead:       lead
+        }
+        $scope.tasks.push(task)
+    })
+
+    // Get all leads
+    $http({
+        method:     'GET',
+        url:        '/api/users/lead',
+        headers:    {
+            'X-Auth-Token':    $localStorage.accessToken
+        }
+    })
+    .then(
+        function (response) {
+            $scope.leads = response.data
+        },
+        function (error) {
+            console.log(error)
+            $state.go('home')
+        }
+    )
+
+    // Get Team
+    $http({
+        method:     'GET',
+        url:        '/api/users/team',
+        headers:    {
+            'X-Auth-Token':    $localStorage.accessToken
+        }
+    })
+    .then(
+        function (response) {
+            $scope.team = response.data
+        },
+        function (error) {
+            console.log(error)
+            $state.go('home')
+        }
+    )
+
+    // Get Form Templates
+    $http({
+        method:     'GET',
+        url:        '/api/users/form',
+        headers:    {
+            'X-Auth-Token':    $localStorage.accessToken
+        }
+    })
+    .then(
+        function (response) {
+            $scope.formTemplates = response.data
+        },
+        function (error) {
+            console.log(error)
+            $state.go('home')
+        }
+    )
+    /* ----------------------------- APIs --------------------------------*/
+    // Button Click APIs
+
+    $scope.add = function () {
+        // Add empty task to array
+        $scope.tasks.push({})
+    }
+
+    $scope.create = function () {
+        // Check if any entries are null
+        $scope.tasks.forEach(function (task) {
+            if (!task.lead || !task.rep || !task.template)
+            {
+                console.log("Empty Entries found")
+                return
+            }
+        })
+
+        // Send to server for saving
+        $http({
+            method:     'POST',
+            url:        '/api/users/task',
+            headers:    {
+                'X-Auth-Token':    $localStorage.accessToken
+            },
+            data:       {
+                tasks:      JSON.stringify($scope.tasks)
+            }
+        })
+        .then(
+            function (response) {
+                $scope.leads = response.data
+            },
+            function (error) {
+                console.log(error)
+                $state.go('home')
+            }
+        )
+    }
 })
