ec05e2763b2ff20a976fb75e4fe5ae20c254d6c7
siddharthg <siddharthg.official@gmail.com>
2017-09-08 15:00:30 +0530

    [EXCEL][EXPORT] Logic to create and save an excel file - Add functionality to export button in reports - Add functionality to Excel Service to convert table to workbook and save to local computer - Hook up buttons to Excel Service in Lead and Team Reports

--- src/main/webapp/js/services/ExcelService.js
+++ src/main/webapp/js/services/ExcelService.js
@@ -2,7 +2,7 @@
  * Created by Siddharth on 26-08-2017.
  */
 
-app.service("ExcelService", function ($http, $localStorage) {
+app.service("ExcelService", function ($http, $localStorage, $filter, FileSaver, Blob) {
 
     this.excelRead = function (workbook) {
         // Get Worksheet
@@ -36,7 +36,46 @@ app.service("ExcelService", function ($http, $localStorage) {
             },
             data:       {
                 excelData:           JSON.stringify(rows)
+            }
+        })
+    }
+
+    // APIs to save table in excel file
+    this.export = function (table, fileNamePrefix){
+        // Convert table to workbook
+        var wb = XLSX.utils.table_to_book(table, {sheet:"Sheet 1"})
+
+        // Create saveable blob object
+        var wbOut = XLSX.write(wb, {bookType:'xlsx', bookSST:true, type: 'binary'})
+        var blob  = new Blob([s2ab(wbOut)],{type:"application/octet-stream"})
+
+        // Get suffix (timestamp) to attach to file name
+        var fileNameSuffix = getFileSuffix()
+        var fileName = fileNamePrefix + fileNameSuffix + ".xlsx"
+
+        try {
+            FileSaver.saveAs(blob, fileName)
+        } catch(e) {
+            console.log(e, wbOut)
         }
-    })
+    }
+
+    function getFileSuffix(){
+        var timestamp = new Date()
+        timestamp = $filter('date')(timestamp, "-dd-MM-yy-HHmm")
+        return timestamp
+    }
+
+    function s2ab(s) {
+        if(typeof ArrayBuffer !== 'undefined') {
+            var buf = new ArrayBuffer(s.length);
+            var view = new Uint8Array(buf);
+            for (var i=0; i!=s.length; ++i) view[i] = s.charCodeAt(i) & 0xFF;
+            return buf;
+        } else {
+            var buf = new Array(s.length);
+            for (var i=0; i!=s.length; ++i) buf[i] = s.charCodeAt(i) & 0xFF;
+            return buf;
+        }
     }
 })
