53bb17430ad1aefe2d6e0c9d8e0faff5a5d06e59
siddharthg <siddharthg.official@gmail.com>
2017-09-08 15:17:19 +0530

    [DASHBOARD][CONTENT][REVIEW] Address review comments : https://github.com/siddharthws/NavimateForBusinessServer/pull/20

--- grails-app/services/navimateforbusiness/ReportService.groovy
+++ grails-app/services/navimateforbusiness/ReportService.groovy
@@ -10,12 +10,12 @@ class ReportService {
 
         // Get reps
         List<User> team = User.findAllByManager(manager)
-        for (User member : team){
+        team.each {member ->
             // Get all tasks for this rep
             List<Task> tasks = Task.findAllByRep(member)
-            for (Task task : tasks){
+            tasks.each {task ->
                 // Add an entry in report for each form in this task
-                for (Form form : task.forms) {
+                task.forms.each {form ->
                     def row = [ rep: task.rep.name,
                                 lead: task.lead.company,
                                 form: form.data]
@@ -24,7 +24,7 @@ class ReportService {
                 }
 
                 // If no forms in this task, add single entry with empty form
-                if (task.forms.size() == 0) {
+                if (task?.forms) {
                     def row = [ rep: task.rep.name,
                                 lead: task.lead.company,
                                 form: ""]
@@ -42,12 +42,12 @@ class ReportService {
 
         // Get all leads for this user
         List<Lead> leads = Lead.findAllByManager(manager)
-        for (Lead lead : leads){
+        leads.each {lead ->
             // Get all tasks for this lead
             List<Task> tasks = Task.findAllByLead(lead)
-            for (Task task : tasks){
+            tasks.each {task ->
                 // Add an entry in report for each form in this task
-                for (Form form : task.forms) {
+                task.forms.each {form ->
                     def row = [ rep: task.rep.name,
                                 lead: task.lead.company,
                                 form: form.data]
@@ -56,7 +56,7 @@ class ReportService {
                 }
 
                 // If no forms in this task, add single entry with empty form
-                if (task.forms.size() == 0) {
+                if (task?.forms) {
                     def row = [ rep: task.rep.name,
                                 lead: task.lead.company,
                                 form: "No Forms Added"]
