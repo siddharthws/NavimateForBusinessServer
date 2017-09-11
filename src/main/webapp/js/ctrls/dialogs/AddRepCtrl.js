1f7f64692b5d7fa60dfec7666391b279bc5e928d
siddharthg <siddharthg.official@gmail.com>
2017-09-11 20:43:15 +0530

    [REP][ADD][DIALOG] Add Data Validation - Validate using angular - Change input field display when error by dynamically adding error class - Launch Error Toast

--- src/main/webapp/js/ctrls/dialogs/AddRepCtrl.js
+++ src/main/webapp/js/ctrls/dialogs/AddRepCtrl.js
@@ -3,13 +3,42 @@
  */
 
 // Controller for Alert Dialog
-app.controller('AddRepCtrl', function ($scope, $mdDialog) {
+app.controller('AddRepCtrl', function ($scope, $mdDialog, ToastService) {
 
     $scope.add = function () {
-        // Placeholder
+        if (validate()) {
+            // Placeholder for success validation
+        }
     }
 
     $scope.cancel = function () {
         $mdDialog.hide()
+    }
+
+    // API to validate entered data
+    function validate () {
+        var bValid = true
+
+        // Reset Error Fields
+        $scope.bNameError = false
+        $scope.bPhoneError = false
+
+        // Validate Name
+        if (!$scope.name) {
+            bValid = false
+            $scope.bNameError = true
+        }
+
+        // Validate number
+        if (!$scope.phoneNumber) {
+            bValid = false
+            $scope.bPhoneError = true
+        }
+
+        if (!bValid) {
+            ToastService.toast("Please fill all fields.")
+        }
+
+        return bValid
     }
 })
