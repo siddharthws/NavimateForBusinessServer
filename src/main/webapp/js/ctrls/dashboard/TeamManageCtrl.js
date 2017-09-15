6f147feaa46e4e65afd46a7273e3ba39b41da5db
siddharthg <siddharthg.official@gmail.com>
2017-09-15 14:02:58 +0530

    [LIST][ACTIONS] Add support for list actions in team management - Add checkbox column to select multiple list items - Add dropdown menu. Keep disabled if no list items are selected - Add logic to toggle selection status

--- src/main/webapp/js/ctrls/dashboard/TeamManageCtrl.js
+++ src/main/webapp/js/ctrls/dashboard/TeamManageCtrl.js
@@ -4,13 +4,17 @@
 
 app.controller("TeamManageCtrl", function ($scope, $http, $localStorage, $state, DialogService) {
 
+    /* ------------------------------- INIT -----------------------------------*/
     // Set menu and option
     $scope.selection.item       = MENU_ITEMS[MENU_ITEM_TEAM]
     $scope.selection.option     = ITEM_OPTIONS[ITEM_OPTION_MANAGE]
 
-    /* ------------------------------- INIT -----------------------------------*/
     $scope.init = function ()
     {
+        // Re-initialize selection to empty
+        $scope.selection = []
+
+        // Get Team Data
         $http({
             method:     'GET',
             url:        '/api/users/team',
@@ -18,15 +22,15 @@ app.controller("TeamManageCtrl", function ($scope, $http, $localStorage, $state,
                 'X-Auth-Token':    $localStorage.accessToken
             }
         })
-            .then(
-                function (response) {
-                    $scope.team = response.data
-                },
-                function (error) {
-                    console.log(error)
-                    $state.go('home')
-                }
-            )
+        .then(
+            function (response) {
+                $scope.team = response.data
+            },
+            function (error) {
+                console.log(error)
+                $state.go('home')
+            }
+        )
     }
 
     // Init View
@@ -35,5 +39,35 @@ app.controller("TeamManageCtrl", function ($scope, $http, $localStorage, $state,
     /* ------------------------------- APIs -----------------------------------*/
     $scope.add = function () {
         DialogService.addRep($scope.init)
+    }
+
+    // Single List Item Selection Toggle
+    $scope.toggleSelection = function (rep) {
+        var idx = $scope.selection.indexOf(rep)
+
+        // Check if rep is present in selection
+        if (idx != -1) {
+            // Remove from selection
+            $scope.selection.splice(idx, 1)
+        } else {
+            // Add in selection
+            $scope.selection.push(rep)
+        }
+    }
+
+    // Full List Selection Toggling
+    $scope.toggleAll = function () {
+        // Check if all are selected
+        if ($scope.selection.length == $scope.team.length) {
+            // Remove All
+            $scope.selection.splice(0, $scope.selection.length)
+        } else {
+            // Add All
+            $scope.team.forEach(function (rep) {
+                if ($scope.selection.indexOf(rep) == -1) {
+                    $scope.selection.push(rep)
+                }
+            })
+        }
     }
 })
