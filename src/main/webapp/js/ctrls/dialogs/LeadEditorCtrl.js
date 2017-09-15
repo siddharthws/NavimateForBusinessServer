0f5344ed38d1df8fb42581735dd9c0fa070b7cc6
siddharthg <siddharthg.official@gmail.com>
2017-09-15 23:26:01 +0530

    [LEAD][EDITOR] Add Lead Editor html with styling - Add html, controller and styling - Add info, lead list, edit box, map layouts etc... to HTML - Add list, button, input styling to dialog components - Open dialog from Lead Management

---
+++ src/main/webapp/js/ctrls/dialogs/LeadEditorCtrl.js
@@ -0,0 +1,25 @@
+/**
+ * Created by Siddharth on 15-09-2017.
+ */
+
+// Controller for Alert Dialog
+app.controller('LeadEditorCtrl', function ($scope, $mdDialog, leads) {
+
+    /* ------------------------------- INIT -----------------------------------*/
+    /* ------------------------------- APIs -----------------------------------*/
+    $scope.add = function () {
+        // Placeholder
+    }
+
+    $scope.upload = function () {
+        // Placeholder
+    }
+
+    $scope.save = function () {
+        // Placeholder
+    }
+
+    $scope.cancel = function () {
+        $mdDialog.hide()
+    }
+})
