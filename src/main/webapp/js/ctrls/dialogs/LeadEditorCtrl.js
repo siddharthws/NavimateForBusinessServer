4883c1c3e2a0d659c443040d049e6598e0221f41
siddharthg <siddharthg.official@gmail.com>
2017-09-16 14:45:51 +0530

    [LEAD][EDITOR] Addition and validation of leads - Initialize data objects in controllers - Use objects to populate HTML elements (list / editbox) - Add logic to mark selected leads (on list item click) and corresponding UI hadnling - Add validation algortithm on save click - Add erorr handling UI (Toast and has-error class)

--- src/main/webapp/js/ctrls/dialogs/LeadEditorCtrl.js
+++ src/main/webapp/js/ctrls/dialogs/LeadEditorCtrl.js
@@ -3,12 +3,20 @@
  */
 
 // Controller for Alert Dialog
-app.controller('LeadEditorCtrl', function ($scope, $mdDialog, leads) {
+app.controller('LeadEditorCtrl', function ($scope, $mdDialog, ToastService, leads) {
 
-    /* ------------------------------- INIT -----------------------------------*/
-    /* ------------------------------- APIs -----------------------------------*/
+    /* ------------------------------- Scope APIs -----------------------------------*/
     $scope.add = function () {
-        // Placeholder
+        // Add empty lead to array
+        var lead = {}
+        $scope.leads.push(lead)
+
+        // Select the newly added lead
+        $scope.selectedLead = lead
+    }
+
+    $scope.listItemClick = function (lead) {
+        $scope.selectedLead = lead
     }
 
     $scope.upload = function () {
@@ -16,10 +24,45 @@ app.controller('LeadEditorCtrl', function ($scope, $mdDialog, leads) {
     }
 
     $scope.save = function () {
-        // Placeholder
+        // Validate entered leads
+        if (validate()) {
+            // Data Validated. Save in database
+        }
+    }
+
+    $scope.isLeadValid = function (lead) {
+        return (lead.company && lead.name && lead.phoneNumber && lead.latitude && lead.longitude && lead.address)
     }
 
     $scope.cancel = function () {
         $mdDialog.hide()
+    }
+
+    /* ------------------------------- Script APIs -----------------------------------*/
+    function validate() {
+        // Reset Error flag
+        $scope.bShowError = false
+
+        // Validate leads
+        $scope.leads.forEach(function (lead) {
+            if (!$scope.isLeadValid(lead)) {
+                $scope.bShowError = true
+            }
+        })
+    }
+
+    /* ------------------------------- INIT -----------------------------------*/
+    // Init objects
+    $scope.leads = []
+    $scope.selectedLead = {}
+    $scope.bShowError = false
+
+    if (leads) {
+        // Assign the passed leads & mark the first one as selected
+        $scope.leads = leads
+        $scope.selectedLead = $scope.leads[0]
+    } else {
+        // Add new
+        $scope.add()
     }
 })
