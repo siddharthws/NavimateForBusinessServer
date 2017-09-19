926346b5b5c0d1646bb915326c7781544ff1c95c
siddharthg <siddharthg.official@gmail.com>
2017-09-19 23:29:50 +0530

    [FORM][VIEW] Add dialog to edit form - Display form data in dialog - Make default values editable - Callback to re0initialize forms when updated - Update Form Management's Edit Button

--- src/main/webapp/js/ctrls/dashboard/FormManageCtrl.js
+++ src/main/webapp/js/ctrls/dashboard/FormManageCtrl.js
@@ -2,27 +2,39 @@
  * Created by Siddharth on 22-08-2017.
  */
 
-app.controller("FormManageCtrl", function ($scope, $http, $localStorage, $state) {
+app.controller("FormManageCtrl", function ($scope, $http, $localStorage, $state, DialogService) {
 
+    /*------------------------------- INIT -------------------------------*/
     // Set menu and option
     $scope.selection.item       = MENU_ITEMS[MENU_ITEM_FORMS]
     $scope.selection.option     = ITEM_OPTIONS[ITEM_OPTION_MANAGE]
 
     // Get Forms for this user
-    $http({
-        method:     'GET',
-        url:        '/api/users/form',
-        headers:    {
-            'X-Auth-Token':    $localStorage.accessToken
-        }
-    })
-    .then(
-        function (response) {
-            $scope.forms = response.data
-        },
-        function (error) {
-            console.log(error)
-            $state.go('home')
-        }
-    )
+    getForms()
+
+    /*------------------------------- Scope APIs -------------------------------*/
+    $scope.edit = function (form) {
+        DialogService.editForm(form, getForms)
+    }
+
+    /*------------------------------- Other APIs -------------------------------*/
+
+    function getForms() {
+        $http({
+            method:     'GET',
+            url:        '/api/users/form',
+            headers:    {
+                'X-Auth-Token':    $localStorage.accessToken
+            }
+        })
+        .then(
+            function (response) {
+                $scope.forms = response.data
+            },
+            function (error) {
+                console.log(error)
+                $state.go('home')
+            }
+        )
+    }
 })
