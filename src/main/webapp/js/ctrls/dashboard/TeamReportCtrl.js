f05ae3da5cd505b00ec9e2639346b05d8718dcbb
siddharthg <siddharthg.official@gmail.com>
2017-09-21 22:21:34 +0530

    [REPORT][FILTER][CLEAR] Add logic to clear filters - Reset the whole filter object - Restructure TeamReportCtrl.js to have Init and Scope APIs sections in correct order

--- src/main/webapp/js/ctrls/dashboard/TeamReportCtrl.js
+++ src/main/webapp/js/ctrls/dashboard/TeamReportCtrl.js
@@ -4,71 +4,7 @@
 
 app.controller("TeamReportCtrl", function ($scope, $http, $localStorage, $state, $filter, ExcelService) {
 
-    // Set menu and option
-    $scope.selection.item       = MENU_ITEMS[MENU_ITEM_TEAM]
-    $scope.selection.option     = ITEM_OPTIONS[ITEM_OPTION_REPORT]
-
-    // Init Report Variables
-    $scope.report = []
-    $scope.filteredReport = []
-
-    // Init Filter
-    $scope.filter = {
-        rep: {
-            selection: []
-        },
-        lead: {
-            selection: []
-        },
-        sales: {
-            lesserThan: '',
-            greaterThan: ''
-        },
-        status: {
-            selection: []
-        },
-        notes: {
-            search: ''
-        },
-        date: {
-            from:   '',
-            to:     ''
-        },
-        sort: []
-    }
-
-    // Get team report
-    $http({
-        method:     'GET',
-        url:        '/api/reports/team',
-        headers:    {
-            'X-Auth-Token':    $localStorage.accessToken
-        }
-    })
-    .then(
-        function (response) {
-            $scope.report = response.data
-            $scope.filteredReport = $scope.report
-        },
-        function (error) {
-            console.log(error)
-            $state.go('home')
-        }
-    )
-
-    // Hack to persist multiselect dropdowns after clicking on dropdown items
-    $('body').on('click', function (e) {
-        if (!$('.dropdown.dropdown-multiselect').is(e.target)
-            && $('.dropdown.dropdown-multiselect').has(e.target).length === 0
-            && $('.show').has(e.target).length === 0) {
-            $('.dropdown.dropdown-multiselect .dropdown-menu').removeClass('show')
-        }
-    })
-
-    $('.dropdown.dropdown-multiselect > button').on('click', function (e) {
-        $($(this).parent()).find('.dropdown-menu').toggleClass('show')
-    })
-
+    /*-------------------------------------- Scope APIs ---------------------------------------*/
     $scope.export = function () {
         // Get table to export
         var table = $('.team-report-table').get(0)
@@ -78,6 +14,33 @@ app.controller("TeamReportCtrl", function ($scope, $http, $localStorage, $state,
     }
 
     // Filter Related APIs
+    $scope.resetFilters = function () {
+        // Init Blank Filter
+        $scope.filter = {
+            rep: {
+                selection: []
+            },
+            lead: {
+                selection: []
+            },
+            sales: {
+                lesserThan: '',
+                greaterThan: ''
+            },
+            status: {
+                selection: []
+            },
+            notes: {
+                search: ''
+            },
+            date: {
+                from:   '',
+                to:     ''
+            },
+            sort: []
+        }
+    }
+
     $scope.filterSort = function(property, bReverse) {
         console.log(property + "  " + bReverse)
         if (bReverse) {
@@ -131,7 +94,6 @@ app.controller("TeamReportCtrl", function ($scope, $http, $localStorage, $state,
 
     // API to apply all filters
     $scope.applyFilters = function () {
-        console.log("Filtering")
         // Reset Report
         $scope.filteredReport = []
 
@@ -153,4 +115,48 @@ app.controller("TeamReportCtrl", function ($scope, $http, $localStorage, $state,
         // Apply sorting
         $scope.filteredReport = $filter('orderBy')($scope.filteredReport, $scope.filter.sort)
     }
+
+    /*----------------------------------- INIT --------------------------------*/
+    // Set menu and option
+    $scope.selection.item       = MENU_ITEMS[MENU_ITEM_TEAM]
+    $scope.selection.option     = ITEM_OPTIONS[ITEM_OPTION_REPORT]
+
+    // Init Report Variables
+    $scope.report = []
+    $scope.filteredReport = []
+
+    // Init filter
+    $scope.resetFilters()
+
+    // Get team report
+    $http({
+        method:     'GET',
+        url:        '/api/reports/team',
+        headers:    {
+            'X-Auth-Token':    $localStorage.accessToken
+        }
+    })
+        .then(
+            function (response) {
+                $scope.report = response.data
+                $scope.filteredReport = $scope.report
+            },
+            function (error) {
+                console.log(error)
+                $state.go('home')
+            }
+        )
+
+    // Hack to persist multiselect dropdowns after clicking on dropdown items
+    $('body').on('click', function (e) {
+        if (!$('.dropdown.dropdown-multiselect').is(e.target)
+            && $('.dropdown.dropdown-multiselect').has(e.target).length === 0
+            && $('.show').has(e.target).length === 0) {
+            $('.dropdown.dropdown-multiselect .dropdown-menu').removeClass('show')
+        }
+    })
+
+    $('.dropdown.dropdown-multiselect > button').on('click', function (e) {
+        $($(this).parent()).find('.dropdown-menu').toggleClass('show')
+    })
 })
