23489ca95fc891d6d9b3778bcdc1cb83a885f3ad
siddharthg <siddharthg.official@gmail.com>
2017-09-21 20:44:49 +0530

    [TEAM][REPORT][SORT] Add logic to sort the table using filters - Add filter object to store sorting parameters - Add logic to add / remove sort filters from UI buttons - Add logic to apply filters

--- src/main/webapp/js/ctrls/dashboard/TeamReportCtrl.js
+++ src/main/webapp/js/ctrls/dashboard/TeamReportCtrl.js
@@ -2,14 +2,20 @@
  * Created by Siddharth on 22-08-2017.
  */
 
-app.controller("TeamReportCtrl", function ($scope, $http, $localStorage, $state, ExcelService) {
+app.controller("TeamReportCtrl", function ($scope, $http, $localStorage, $state, $filter, ExcelService) {
 
     // Set menu and option
     $scope.selection.item       = MENU_ITEMS[MENU_ITEM_TEAM]
     $scope.selection.option     = ITEM_OPTIONS[ITEM_OPTION_REPORT]
 
-    // Init Variables
+    // Init Report Variables
     $scope.report = []
+    $scope.filteredReport = []
+
+    // Init Filter
+    $scope.filter = {
+        sort: []
+    }
 
     // Get team report
     $http({
@@ -22,6 +28,7 @@ app.controller("TeamReportCtrl", function ($scope, $http, $localStorage, $state,
     .then(
         function (response) {
             $scope.report = response.data
+            $scope.filteredReport = $scope.report
         },
         function (error) {
             console.log(error)
@@ -48,5 +55,48 @@ app.controller("TeamReportCtrl", function ($scope, $http, $localStorage, $state,
 
         // Call Excel Service
         ExcelService.export(table, "Team-Report")
+    }
+
+    // Filter Related APIs
+    $scope.filterSort = function(property, bReverse) {
+        console.log(property + "  " + bReverse)
+        if (bReverse) {
+            // Remove forward if present
+            if ($scope.filter.sort.indexOf(property) != -1) {
+                $scope.filter.sort.splice($scope.filter.sort.indexOf(property), 1)
+            }
+
+            property = '-' + property
+        } else {
+            // Remove reverse if present
+            if ($scope.filter.sort.indexOf('-' + property) != -1) {
+                $scope.filter.sort.splice($scope.filter.sort.indexOf('-' + property), 1)
+            }
+        }
+
+        // Add / remove property
+        if ($scope.filter.sort.indexOf(property) != -1) {
+            $scope.filter.sort.splice($scope.filter.sort.indexOf(property), 1)
+        } else {
+            $scope.filter.sort.push(property)
+        }
+
+        // Re-apply filters
+        $scope.applyFilters()
+    }
+
+    // API to apply all filters
+    $scope.applyFilters = function () {
+        console.log("Filtering")
+        // Reset Report
+        $scope.filteredReport = []
+
+        // Apply select filters
+        $scope.report.forEach(function (row) {
+            $scope.filteredReport.push(row)
+        })
+
+        // Apply sorting
+        $scope.filteredReport = $filter('orderBy')($scope.filteredReport, $scope.filter.sort)
     }
 })
