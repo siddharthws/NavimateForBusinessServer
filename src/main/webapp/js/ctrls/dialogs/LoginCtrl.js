cae5587d1d9e5eac9ea84fbb5f845ff376906b5e
siddharthg <siddharthg.official@gmail.com>
2017-09-22 00:44:55 +0530

    [LOGIN][STYLING] Add styling to Login dialog - Add basic dialog structure - Add underlined inputs - Add error handling on UI - Add register now link which opens registration dialog

--- src/main/webapp/js/ctrls/dialogs/LoginCtrl.js
+++ src/main/webapp/js/ctrls/dialogs/LoginCtrl.js
@@ -2,28 +2,57 @@
  * Created by Siddharth on 01-09-2017.
  */
 
-app.controller('LoginCtrl', function ($scope, $mdDialog, $state, $localStorage, AuthService) {
+app.controller('LoginCtrl', function ($scope, $mdDialog, $state, $localStorage, AuthService, DialogService, ToastService) {
 
+    /* ------------------------------- Scope APIs -----------------------------------*/
+    // Button Click APIs
     $scope.login = function(){
 
-        AuthService.login($scope.phoneNumber, $scope.password)
-            .then(
-                function (response) {
-                    // Hide Dialog
-                    $mdDialog.hide()
+        // Validate credentials
+        if (validate()) {
+            AuthService.login($scope.phoneNumber, $scope.password)
+                .then(
+                    function (response) {
+                        // Hide Dialog
+                        $mdDialog.hide()
 
-                    // Redirect to dashboard
-                    $localStorage.accessToken = response.data.accessToken;
-                    $state.go("dashboard.team-manage")
-                },
-                function (error) {
-                    // Login Failure
-                    console.log(error)
-                }
-            )
+                        // Redirect to dashboard
+                        $localStorage.accessToken = response.data.accessToken;
+                        $state.go("dashboard.team-manage")
+                    },
+                    function (error) {
+                        // Show error toast
+                        ToastService.toast("Invalid Credentials...")
+                    }
+                )
+        }
+    }
+
+    $scope.register = function () {
+        // Launch register dialog
+        DialogService.register()
     }
 
     $scope.cancel = function () {
         $mdDialog.hide()
     }
+
+    /* ------------------------------- Local APIs -----------------------------------*/
+    function validate() {
+        if (!$scope.phoneNumber || !$scope.password) {
+            // Raise error flag
+            $scope.bShowError = true
+
+            // Show error toast
+            ToastService.toast("Please fill all fields !!!")
+
+            return false
+        }
+
+        return true
+    }
+
+    /* ------------------------------- INIT -----------------------------------*/
+    $scope.bShowError = false;
+
 })
