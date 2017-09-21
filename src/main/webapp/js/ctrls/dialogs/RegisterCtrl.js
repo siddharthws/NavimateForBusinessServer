5a641ddb3e900ddb7324d0c4dc036adfca225ece
siddharthg <siddharthg.official@gmail.com>
2017-09-22 00:56:14 +0530

    [REGISTER][STYLING] Add styling to Register dialog - Add basic dialog structure - Add underlined inputs - Add error handling on UI

--- src/main/webapp/js/ctrls/dialogs/RegisterCtrl.js
+++ src/main/webapp/js/ctrls/dialogs/RegisterCtrl.js
@@ -2,25 +2,51 @@
  * Created by Siddharth on 01-09-2017.
  */
 
-app.controller('RegisterCtrl', function ($scope, $mdDialog, AuthService, DialogService) {
+app.controller('RegisterCtrl', function ($scope, $mdDialog, AuthService, DialogService, ToastService) {
 
+    /* ------------------------------- Scope APIs -----------------------------------*/
+    // Button Click APIs
     $scope.register = function(){
-        AuthService.register($scope.name, $scope.phoneNumber, $scope.password)
-            .then(
-                function (response) {
-                    // Close this dialog
-                    $mdDialog.hide()
 
-                    // Open Login Dialog
-                    DialogService.login()
-                },
-                function (error) {
-                    console.log(error)
-                }
-            )
+        // Validate credentials
+        if (validate()) {
+            AuthService.register($scope.name, $scope.phoneNumber, $scope.password)
+                .then(
+                    function (response) {
+                        // Close this dialog
+                        $mdDialog.hide()
+
+                        // Open Login Dialog
+                        DialogService.login()
+                    },
+                    function (error) {
+                        // Show error toast
+                        ToastService.toast("Unable to register...")
+                    }
+                )
+        }
     }
 
     $scope.cancel = function () {
         $mdDialog.hide()
     }
+
+    /* ------------------------------- Local APIs -----------------------------------*/
+    function validate() {
+        if (!$scope.phoneNumber || !$scope.password || !$scope.name) {
+            // Raise error flag
+            $scope.bShowError = true
+
+            // Show error toast
+            ToastService.toast("Please fill all fields !!!")
+
+            return false
+        }
+
+        return true
+    }
+
+    /* ------------------------------- INIT -----------------------------------*/
+    $scope.bShowError = false;
+
 })
