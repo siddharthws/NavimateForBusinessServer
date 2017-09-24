ea8234647f901a8971c3fdb986be1a426533a6cd
siddharthg <siddharthg.official@gmail.com>
2017-09-24 23:48:23 +0530

    [APP][REP][FCM] Add support to send fcm messages on adding tasks - fcm service to send app notifications - uses fcm server and key to connect to fcm - Used in addTasks to send fcm messages to reps

---
+++ grails-app/services/navimateforbusiness/FcmService.groovy
@@ -0,0 +1,30 @@
+package navimateforbusiness
+
+import grails.gorm.transactions.Transactional
+import grails.plugins.rest.client.RestBuilder
+
+@Transactional
+class FcmService {
+
+    def notifyApp (fcmId) {
+        if (fcmId) {
+            // Send request to FCM server
+            def request = new RestBuilder(connectTimeout:1000, readTimeout:20000)
+            def resp = request.post("https://fcm.googleapis.com/fcm/send") {
+                header 'Content-Type', 'application/json'
+                header 'Authorization', 'key=AAAA2Ezvs_Y:APA91bFgkZh16BJHk0IZlkwXQAAJb6HwNC4svq0Q8knmkiyXTZOR9xGZ-QARpKUIDuuIJEvZq9UMfOPfYnUtpawLCr9Vrju32mk8dBYatBu3Rp7mSeuDb3pKfUr9ZOc6dTTdckYmd7hq'
+                json {
+                    data = {
+                        type = 1
+                    }
+                    to = fcmId
+                }
+            }
+
+            // Validate response
+            if (!resp.json.success) {
+                log.error("Error while sending FCM : " + resp.json)
+            }
+        }
+    }
+}
---
+++ src/test/groovy/navimateforbusiness/FcmServiceSpec.groovy
@@ -0,0 +1,18 @@
+package navimateforbusiness
+
+import grails.testing.services.ServiceUnitTest
+import spock.lang.Specification
+
+class FcmServiceSpec extends Specification implements ServiceUnitTest<FcmService>{
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
