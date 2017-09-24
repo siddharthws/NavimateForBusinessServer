4e3407dcce08a605cedab499192e243de4499395
siddharthg <siddharthg.official@gmail.com>
2017-09-24 20:59:45 +0530

    [APP][REP][PROFILE] Add backend api to return a registered rep's profile - GET api to return rep information or throw exception

---
+++ src/test/groovy/navimateforbusiness/api/RepApiControllerSpec.groovy
@@ -0,0 +1,18 @@
+package navimateforbusiness.api
+
+import grails.testing.web.controllers.ControllerUnitTest
+import spock.lang.Specification
+
+class RepApiControllerSpec extends Specification implements ControllerUnitTest<RepApiController> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
