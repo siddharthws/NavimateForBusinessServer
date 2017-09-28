1c55d256ad4a0883ac7f4831f4b1e330f1fc0a98
rhnmahajan83 <rhnmahajan83@gmail.com>
2017-09-29 04:49:26 +0530

    [TASK][MANAGE] added tasks close functionality -added checkboxes to TaskManage -Action button added with dropdown list containing close selection -on selecting close status of selected tasks is changed to closed

--- src/main/webapp/js/ctrls/dashboard/TaskManageCtrl.js
+++ src/main/webapp/js/ctrls/dashboard/TaskManageCtrl.js
@@ -2,12 +2,15 @@
  * Created by Siddharth on 22-08-2017.
  */
 
-app.controller("TaskManageCtrl", function ($scope, $http, $localStorage, $state, DialogService) {
+app.controller("TaskManageCtrl", function ($scope, $http, $localStorage, $state, DialogService, ToastService) {
 
     /*-------------------------------- INIT --------------------------------*/
         // Set menu and option
     $scope.selection.item       = MENU_ITEMS[MENU_ITEM_TASKS]
     $scope.selection.option     = ITEM_OPTIONS[ITEM_OPTION_MANAGE]
+
+    //Re-initialize selection to empty
+    $scope.selection = []
 
     function initTasks () {
         $http({
@@ -33,5 +36,69 @@ app.controller("TaskManageCtrl", function ($scope, $http, $localStorage, $state,
     $scope.add = function () {
         // Launch Task Creator dialog
         DialogService.taskCreator(initTasks)
+    }
+
+    // Single List Item Selection Toggle
+    $scope.toggleSelection = function (task) {
+        var idx = $scope.selection.indexOf(task)
+
+        // Check if task is present in selection
+        if (idx != -1) {
+            // Remove from selection
+            $scope.selection.splice(idx, 1)
+        } else {
+            // Add in selection
+            $scope.selection.push(task)
+        }
+    }
+
+    // Full List Selection Toggling
+    $scope.toggleAll = function () {
+        // Check if all are selected
+        if ($scope.selection.length == $scope.tasks.length) {
+            // Remove All
+            $scope.selection.splice(0, $scope.selection.length)
+        } else {
+            // Add All
+            $scope.tasks.forEach(function (task) {
+                if ($scope.selection.indexOf(task) == -1) {
+                    $scope.selection.push(task)
+                }
+            })
+        }
+    }
+
+    $scope.close = function () {
+        //Launch confirm Dialog box
+        DialogService.confirm("Are you sure you want to close these " + $scope.selection.length + " tasks ?",
+            closeSelected)
+    }
+
+    function closeSelected() {
+        //http call to close tasks
+        $http({
+            method: 'POST',
+            url:    '/api/users/task/close',
+            headers: {
+                'X-Auth-Token': $localStorage.accessToken
+            },
+            data: {
+                tasks : $scope.selection
+            }
+        }).then(
+            function (response) {
+                // Show Toast
+                ToastService.toast("tasks closed...")
+
+                // re-initialize tasks
+                initTasks()
+            },
+            function (error) {
+                ToastService.toast("Failed to close tasks!!!")
+
+                // re-initialize tasks
+                initTasks()
+            })
+
     }
 })
