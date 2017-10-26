2c75b574738d8f58b0f445a0918e100a764ff43e
siddharthg <siddharthg.official@gmail.com>
2017-10-26 19:30:01 +0530

    [REL] Minification process

---
+++ Gruntfile.js
@@ -0,0 +1,40 @@
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            options: {
+                report: 'min',
+                mangle: false
+            },
+            my_target: {
+                files: {'src/main/webapp/app.min.js' : ['src/main/webapp/js/app.js' ,
+                    'src/main/webapp/js/ctrls/**/*.js',
+                    'src/main/webapp/js/macros/*.js',
+                    'src/main/webapp/js/services/*.js']}
+            }
+        },
+        cssmin: {
+            options: {
+                mergeIntoShorthands: false,
+                roundingPrecision: -1
+            },
+            my_target: {
+                files: {
+                    'src/main/webapp/app.min.css': ['src/main/webapp/css/*.css',
+                        'src/main/webapp/css/core/*.css',
+                        'src/main/webapp/css/dashboard/*.css',
+                        'src/main/webapp/css/dialogs/*.css']
+                }
+            }
+        }
+    });
+
+    // Load the plugin that provides the "uglify" task.
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+    // Default task(s).
+    grunt.registerTask('default', ['uglify', 'cssmin']);
+};
