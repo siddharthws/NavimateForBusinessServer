5977a59fe0b63645a8e578ed16ba37415f6512d3
siddharthg <siddharthg.official@gmail.com>
2017-10-26 19:30:01 +0530

    [REL] Minification process

---
+++ minify.cmd
@@ -0,0 +1,6 @@
+call npm install -g
+call npm install -g grunt-cli
+call npm install grunt --save-dev
+call npm install grunt-contrib-cssmin --save-dev
+call npm install grunt-contrib-uglify --save-dev
+grunt
