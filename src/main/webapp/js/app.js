a2a075a8e5c8b69aa224079cdd47f395dfc19ac9
siddharthg <siddharthg.official@gmail.com>
2017-11-06 20:30:24 +0530

    [BUGFIX] Remove input type='number' error

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -192,3 +192,17 @@ app.config(function ($stateProvider, $urlRouterProvider, $httpProvider) {
         controller: 'FormManageCtrl'
     })
 });
+
+app.directive('stringToNumber', function() {
+    return {
+        require: 'ngModel',
+        link: function(scope, element, attrs, ngModel) {
+            ngModel.$parsers.push(function(value) {
+                return '' + value;
+            });
+            ngModel.$formatters.push(function(value) {
+                return parseFloat(value);
+            });
+        }
+    };
+});
