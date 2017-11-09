f612fa01af7815e7dd3a4c7866f61dd1f4e55abc
siddharthg <siddharthg.official@gmail.com>
2017-11-09 20:12:10 +0530

    [TRACKING][FCM] Add support to send FCM messages to start tracking

--- grails-app/services/navimateforbusiness/FcmService.groovy
+++ grails-app/services/navimateforbusiness/FcmService.groovy
@@ -27,4 +27,26 @@ class FcmService {
             }
         }
     }
+
+    def trackApp (fcmId) {
+        if (fcmId) {
+            // Send request to FCM server
+            def request = new RestBuilder(connectTimeout:1000, readTimeout:20000)
+            def resp = request.post("https://fcm.googleapis.com/fcm/send") {
+                header 'Content-Type', 'application/json'
+                header 'Authorization', 'key=AAAA2Ezvs_Y:APA91bFgkZh16BJHk0IZlkwXQAAJb6HwNC4svq0Q8knmkiyXTZOR9xGZ-QARpKUIDuuIJEvZq9UMfOPfYnUtpawLCr9Vrju32mk8dBYatBu3Rp7mSeuDb3pKfUr9ZOc6dTTdckYmd7hq'
+                json {
+                    data = {
+                        type = 2
+                    }
+                    to = fcmId
+                }
+            }
+
+            // Validate response
+            if (!resp.json.success) {
+                log.error("Error while sending track FCM : " + resp.json)
+            }
+        }
+    }
 }
