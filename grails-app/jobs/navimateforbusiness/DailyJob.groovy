4f325a126040acd6a2ea17b92aa2566bad10f7dc
rhnmahajan83 <rhnmahajan83@gmail.com>
2017-11-13 12:06:16 +0530

    [Task][Manage] added logic to daily job -added logic to close the finished tasks -added logic to create a new periodic task with previously finished task details

--- grails-app/jobs/navimateforbusiness/DailyJob.groovy
+++ grails-app/jobs/navimateforbusiness/DailyJob.groovy
@@ -1,5 +1,7 @@
 package navimateforbusiness
 
+import navimateforbusiness.Task
+
 class DailyJob {
     static triggers = {
         //declared a trigger with time 00:01:00 of everyday
@@ -7,6 +9,34 @@ class DailyJob {
     }
 
     void execute() {
+        def currentDate = (new Date()).format("yyyy-MM-dd")
 
+        def tasks = Task.findAll ()
+        tasks.each {task ->
+            if((task.period != 0) && (task.manager !=  null)){
+                //create a new task if old task is expired
+                def dateCreated = task.dateCreated + task.period
+                def expiryDate = dateCreated.format("yyyy-MM-dd")
+                if(expiryDate <= currentDate) {
+                    //Creating new task with previous details
+                    Task newTask = new Task(
+                            account:    task.account,
+                            manager:    task.manager,
+                            rep:        task.rep,
+                            lead:       task.lead,
+                            template:   task.template,
+                            period:     task.period,
+                            status:     navimateforbusiness.TaskStatus.OPEN
+                    )
+                    newTask.save(flush: true, failOnError: true)
+
+                    //closed the finished task and set period to zero
+                    task.status = TaskStatus.CLOSED
+                    task.period = 0
+                    task.save(flush: true, failOnError: true)
+                }
+            }
+        }
     }
 }
+
