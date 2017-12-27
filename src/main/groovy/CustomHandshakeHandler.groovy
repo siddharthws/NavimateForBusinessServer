a8e24a6771800fe27d981b99eb56d85be26c7c56
siddharthg <siddharthg.official@gmail.com>
2017-12-27 13:45:05 +0530

    [WEBSOCKETS] Fix for websockets in production server - Set Request Upgrade Strategy to Tomcat for handshaking - Update SockJs Dependency - Set Allowed origins for all

--- src/main/groovy/CustomHandshakeHandler.groovy
+++ src/main/groovy/CustomHandshakeHandler.groovy
@@ -2,6 +2,7 @@ package navimateforbusiness
 
 import org.springframework.http.server.ServerHttpRequest
 import org.springframework.web.socket.WebSocketHandler
+import org.springframework.web.socket.server.RequestUpgradeStrategy
 import org.springframework.web.socket.server.support.DefaultHandshakeHandler
 
 import java.security.Principal
@@ -10,6 +11,11 @@ import java.security.Principal
  * Created by Siddharth on 22-12-2017.
  */
 class CustomHandshakeHandler extends DefaultHandshakeHandler {
+    // Constructor for tomcat request upgrade strategy
+    CustomHandshakeHandler(RequestUpgradeStrategy strategy) {
+        super(strategy)
+    }
+
     @Override
     protected Principal determineUser(ServerHttpRequest request,
                                       WebSocketHandler wsHandler,
