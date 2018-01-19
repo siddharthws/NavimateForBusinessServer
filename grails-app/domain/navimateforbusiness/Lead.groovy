9f8a49141f7519074cd404604c6cb992ec6a035a
siddharthg <siddharthg.official@gmail.com>
2018-01-19 17:38:26 +0530

    [CLEANUP] Add cleanup after porting DB for

--- grails-app/domain/navimateforbusiness/Lead.groovy
+++ grails-app/domain/navimateforbusiness/Lead.groovy
@@ -23,24 +23,13 @@ class Lead {
     // Templated Data
     Data templateData
 
-    // Deprecated
-    String description
-    String phone
-    String email
-
     static belongsTo = [
             account: Account,
             manager: User
     ]
 
     static constraints = {
-        manager         nullable: true
-        title           nullable: true
-        email           nullable: true
-        latitude        nullable: true
-        longitude       nullable: true
         extId           nullable: true
-        templateData    nullable: true
     }
 
     static mapping = {
