ae6c7f2231b196316e249cb94113c8eaa6d0d984
siddharthg <siddharthg.official@gmail.com>
2018-01-24 21:40:59 +0530

    [APP][VERSION] Update min app version

--- grails-app/controllers/navimateforbusiness/api/AppApiController.groovy
+++ grails-app/controllers/navimateforbusiness/api/AppApiController.groovy
@@ -3,7 +3,7 @@ package navimateforbusiness.api
 import grails.converters.JSON
 
 class AppApiController {
-    static final int MIN_APP_VERSION_CODE = 10
+    static final int MIN_APP_VERSION_CODE = 11
 
     def checkForUpdate() {
         int appVersionCode = request.JSON.versionCode
