880f772249eee2c68bdab7818f80a1773903d0b6
siddharthg <siddharthg.official@gmail.com>
2018-01-26 15:31:13 +0530

    [BUGFIX] Handle error where task does not contain a valid Lead ID

--- src/main/webapp/js/ctrls/dashboard/TaskCloseCtrl.js
+++ src/main/webapp/js/ctrls/dashboard/TaskCloseCtrl.js
@@ -179,7 +179,14 @@ app.controller("TaskCloseCtrl", function ($scope, $rootScope, $http, $localStora
 
             // Add mandatory data to row
             row[0] = task.cId
-            row[1] = LeadDataService.getById(task.leadId).title
+
+            // Add lead data
+            var lead = LeadDataService.getById(task.leadId)
+            // Ignore task if lead is not available
+            if (!lead) {
+                return
+            }
+            row[1] = lead.title
 
             // Add rep Data
             var rep = TeamDataService.getById(task.repId)
