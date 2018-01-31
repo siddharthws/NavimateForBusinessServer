d4ecdbce147e3476eb24d15bb029bf7ef40a90b5
siddharthg <siddharthg.official@gmail.com>
2018-01-31 17:59:39 +0530

    [ACCOUNT][SETTINGS] Add support for saving account settings - Add start time of day and end time of day account settings - Add migration file for db update - Add logic in registration API to create a default account settings object for each new account

---
+++ src/test/groovy/navimateforbusiness/AccountSettingsSpec.groovy
@@ -0,0 +1,18 @@
+package navimateforbusiness
+
+import grails.testing.gorm.DomainUnitTest
+import spock.lang.Specification
+
+class AccountSettingsSpec extends Specification implements DomainUnitTest<AccountSettings> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
