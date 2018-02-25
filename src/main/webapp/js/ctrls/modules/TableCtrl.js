0e233fb2ca7217c3ed5f5a7f9e0da05c98c83676
siddharthg <siddharthg.official@gmail.com>
2018-02-25 18:48:39 +0530

    [DSO][TABLE][DATA] Add support for syncing and showing data through new infra - Add table related parameters to store rows and column - Add logic to sync data from backend - Add helper methods in backend to convert data to tabular format

--- src/main/webapp/js/ctrls/modules/TableCtrl.js
+++ src/main/webapp/js/ctrls/modules/TableCtrl.js
@@ -2,11 +2,66 @@
  * Created by Siddharth on 24-02-2018.
  */
 
-app.controller('TableCtrl', function (TableService) {
+app.controller('TableCtrl', function (  $scope, $window, $state,
+                                        TableService, DialogService) {
     /* ----------------------------- INIT --------------------------------*/
     var vm  = this
 
+    // Add Constants
+    vm.TableConst = Constants.Table
+    vm.FilterConst = Constants.Filter
+    vm.TemplateConst = Constants.Template
+
+    // Get table object from service
+    vm.table = TableService.activeTable
+
+    // Sync table data if required
+    if (!vm.table.columns.length) {
+        sync(true)
+    }
+
     /* ----------------------------- Public APIs --------------------------------*/
+
+    // API to show photo on new page
+    vm.showImage = function (filename) {
+        $window.open($state.href('photos', {name: filename}), "_blank")
+    }
+
+    // API to show location on map
+    vm.showLocation = function (latlng) {
+        // Split location into lat & lng
+        var latLngArr = latlng.split(',')
+
+        // Prepare Location array to send to location viewer
+        var locations = []
+        locations.push({
+            title:      "Picked Location",
+            latitude:   latLngArr[0],
+            longitude:  latLngArr[1]
+        })
+
+        // Open Location Viewer dialog
+        DialogService.locationViewer(locations)
+    }
+
     /* ----------------------------- Private APIs --------------------------------*/
+    // Method to re-initialize data
+    function sync(bColumns) {
+        // Start Async Request to get data
+        vm.table.sync(bColumns).then(
+            // Success Callback
+            function (success) {
+            },
+            // Error Callback
+            function (error) {
+            }
+        )
+    }
+
     /* ----------------------------- Event Listeners --------------------------------*/
+    // Re sync data when Sync Event is triggered
+    $scope.$on(Constants.Events.TABLE_SYNC, function (event, args) {
+        // Sync data
+        sync(false)
+    })
 })
--- src/main/webapp/js/objects/ObjTable.js
+++ src/main/webapp/js/objects/ObjTable.js
@@ -2,13 +2,120 @@
  * Created by Siddharth on 25-02-2018.
  */
 
-app.factory('ObjTable', function() {
+app.factory('ObjTable', function($http, $q, $localStorage) {
     ObjTable = function (type) {
         // ----------------------------------- INIT ------------------------------------//
         var vm = this
 
+        // Type fo table
+        vm.type = Constants.Table.TYPE_INVALID
+
+        // Rows and columns
+        vm.columns = []
+        vm.rows = []
+
+        // Sync request serializing logic
+        var syncCanceller = null
+        var bSyncOngoing = false
+
         // ----------------------------------- Public Methods ------------------------------------//
+        vm.sync = function (bColumns) {
+            // Cancel ongoing sync & create new canceller
+            if (bSyncOngoing) {
+                syncCanceller.resolve()
+                bSyncOngoing = false
+            }
+            syncCanceller = $q.defer()
+
+            // Defer object to resolve request
+            var deferred = $q.defer()
+
+            // Trigger http request to sync data
+            bSyncOngoing = true
+            $http({
+                method:     'POST',
+                url:        "/api/manager/" + Constants.Table.URL_PREFIX[vm.type] + "/getTable",
+                headers:    {
+                    'X-Auth-Token':    $localStorage.accessToken
+                },
+                data:       {
+                    bColumns: bColumns
+                }
+            }).then(
+                function (response) {
+                    // Mark sync complete flag
+                    bSyncOngoing = false
+
+                    // Reset active table columns and rows
+                    handleSyncResult(response.data)
+
+                    // Resolve promise
+                    deferred.resolve(null)
+                },
+                function (error) {
+                    // Ignore if request was cancelled
+                    if (error.status == -1) {
+                        return
+                    }
+
+                    // Mark sync complete flag
+                    bSyncOngoing = false
+
+                    // Reject promise
+                    deferred.reject(null)
+                })
+
+            // Return promise
+            return deferred.promise
+        }
+
         // ----------------------------------- Private Methods ------------------------------------//
+        // Method to handle server data after sync request
+        function handleSyncResult(data) {
+            // Set columns
+            if (data.columns) {
+                // Reset current columns
+                vm.columns = data.columns
+            }
+
+            // Set rows
+            vm.rows         = data.rows
+
+            // Update column sizes
+            refreshColSizes()
+        }
+
+        // Method to refresh column sizes as per latest data
+        function refreshColSizes () {
+            // Iterate through each column
+            vm.columns.forEach(function (column) {
+                // Set sizing for Location / Image columns as small
+                if (column.type == Constants.Template.FIELD_TYPE_SIGN ||
+                    column.type == Constants.Template.FIELD_TYPE_PHOTO ||
+                    column.type == Constants.Template.FIELD_TYPE_LOCATION) {
+                    column.size = Constants.Table.COL_SIZE_S
+                } else {
+                    // Get total number of character in columns
+                    var charCount = column.name.length
+                    vm.rows.forEach(function (row) {
+                        var value = row.values[column.id]
+                        charCount += value.length
+                    })
+
+                    // Get average character per row
+                    var charAvg = charCount / (vm.rows.length + 1)
+
+                    // Set column sizing as per max length
+                    if (charAvg < 5) {
+                        column.size = Constants.Table.COL_SIZE_S
+                    } else if (charAvg < 20) {
+                        column.size = Constants.Table.COL_SIZE_M
+                    } else {
+                        column.size = Constants.Table.COL_SIZE_L
+                    }
+                }
+            })
+        }
     }
 
     return ObjTable
