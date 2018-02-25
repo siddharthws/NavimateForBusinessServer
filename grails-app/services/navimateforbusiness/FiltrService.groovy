6dcc4c676faee563bcd178614bb5e24f37fbcd70
siddharthg <siddharthg.official@gmail.com>
2018-02-25 16:54:56 +0530

    [DSO][PREP] Add empty placeholder files for implementing new table infra - Add table service and object for frontend - Add filter / paging / sorting / table services and backend - Update grunt file to include objects

---
+++ grails-app/services/navimateforbusiness/FiltrService.groovy
@@ -0,0 +1,10 @@
+package navimateforbusiness
+
+import grails.gorm.transactions.Transactional
+
+@Transactional
+class FiltrService {
+    // ----------------------- Dependencies ---------------------------//
+    // ----------------------- Public APIs ---------------------------//
+    // ----------------------- Private APIs ---------------------------//
+}
---
+++ grails-app/services/navimateforbusiness/TableService.groovy
@@ -0,0 +1,10 @@
+package navimateforbusiness
+
+import grails.gorm.transactions.Transactional
+
+@Transactional
+class TableService {
+    // ----------------------- Dependencies ---------------------------//
+    // ----------------------- Public APIs ---------------------------//
+    // ----------------------- Private APIs ---------------------------//
+}
