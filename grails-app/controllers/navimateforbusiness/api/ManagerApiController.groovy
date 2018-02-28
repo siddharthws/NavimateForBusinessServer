06cf387aa3fc2174c86fadc0d4820a4bc0274ce9
aroha_blue <amit42308@gmail.com>
2018-03-01 01:00:08 +0530

    [TASK][EXPORT] Add export support for leads

--- grails-app/controllers/navimateforbusiness/api/ManagerApiController.groovy
+++ grails-app/controllers/navimateforbusiness/api/ManagerApiController.groovy
@@ -175,6 +175,37 @@ class ManagerApiController {
         render resp as JSON
     }
 
+    def exportTasks() {
+        // Get user object
+        def user = authService.getUserFromAccessToken(request.getHeader("X-Auth-Token"))
+
+        // Get filters from request
+        def filter = request.JSON.filter
+        def exportParams = request.JSON.exportParams
+
+        // get tasks for this user
+        def tasks = taskService.getForUser(user)
+
+        // Convert tasks to tabular format
+        def table = tableService.parseTasks(user, tasks)
+
+        // Apply column filters to table
+        table.rows = filtrService.applyToTable(table.rows, filter.colFilters)
+
+        // Apply sorting to table
+        table.rows = sortingService.sortRows(table.rows, filter.sortList)
+
+        // Export table
+        def exportData = tableService.getExportData(table, exportParams)
+
+        // Set response parameters
+        response.setHeader("Content-disposition", "attachment; filename=exportfile.xls")
+        response.contentType = grailsApplication.config.getProperty("grails.mime.types.xls")
+
+        // Export data
+        exportService.export('csv', response.outputStream, exportData.objects, exportData.fields, exportData.labels, [:], [:])
+    }
+
     // ----------------------- FORM APIs ----------------------- //
 
     def getFormTable() {
