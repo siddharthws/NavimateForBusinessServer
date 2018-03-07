0c67262352803da1a12342f2d613cf0b7bee0187
siddharthg <siddharthg.official@gmail.com>
2018-03-07 14:57:37 +0530

    [BUGFIX] Miscellaneous bugfixes and cleanups - Update Input box UI - use canceller promise in table data sync - Add pager validation in backend pagination

--- grails-app/services/navimateforbusiness/PagingService.groovy
+++ grails-app/services/navimateforbusiness/PagingService.groovy
@@ -12,6 +12,11 @@ class PagingService {
         // Get number of objects to be returned
         def count = Math.min(pager.count, objects.size() - pager.startIdx)
 
+        // Validate count
+        if (count < 0) {
+            throw new navimateforbusiness.ApiException("Invalid pager", navimateforbusiness.Constants.HttpCodes.BAD_REQUEST)
+        }
+
         // Populate array with paged objects
         for (int i = 0; i < count; i++) {
             pagedObjects.push(objects[i + pager.startIdx])
