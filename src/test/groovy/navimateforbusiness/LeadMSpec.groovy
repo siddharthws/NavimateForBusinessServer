11356536517aa4becd0ba6066f2023b5716b15fe
siddharthg <siddharthg.official@gmail.com>
2018-04-17 18:00:07 +0530

    [MONGODB][LEADS] Add Lead Domain for Mongo DB - Add domain class - Add Porting API to port PostgreSQL Data to Mongo DB

---
+++ src/test/groovy/navimateforbusiness/LeadMSpec.groovy
@@ -0,0 +1,18 @@
+package navimateforbusiness
+
+import grails.testing.gorm.DomainUnitTest
+import spock.lang.Specification
+
+class LeadMSpec extends Specification implements DomainUnitTest<LeadM> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
