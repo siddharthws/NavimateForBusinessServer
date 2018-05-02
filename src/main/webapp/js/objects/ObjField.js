7676b4069bcd86c8d070e310c44913c95a74f7be
aroha_blue <amit42308@gmail.com>
2018-05-03 00:41:25 +0530

    [TEMPLATE][FIELD] Added Setting Functionality for Field. Mandatory field support added.  § Setting button for each field  § Dialog to add setting  § Checkbox for mandatory setting  § ObjFieldSetting in Frontend

--- src/main/webapp/js/objects/ObjField.js
+++ src/main/webapp/js/objects/ObjField.js
@@ -2,13 +2,14 @@
  * Created by Siddharth on 10-03-2018.
  */
 
-app.factory('ObjField', function() {
+app.factory('ObjField', function(ObjFieldSettings) {
     // ----------------------------------- Constructor ------------------------------------//
     function ObjField (id, title, type, value) {
         this.id = id
         this.type = type
         this.title = title
         this.value = value
+        this.settings = new ObjFieldSettings(false)
     }
 
     // ----------------------------------- Public APIs ------------------------------------//
@@ -16,29 +17,37 @@ app.factory('ObjField', function() {
     ObjField.prototype.Clone = function () {
         // Create clone for value
         var value = JSON.parse(JSON.stringify(this.value))
-
         // Return clone of field object
-        return new ObjField(this.id, this.title, this.type, value)
+        var field = new ObjField(this.id, this.title, this.type, value)
+        field.settings =  this.settings.Clone()
+
+        return field
     }
 
     // ----------------------------------- Private APIs ------------------------------------//
     // ----------------------------------- Static APIs ------------------------------------//
     // Methods to convert between Frontend Field Object and JSON
     ObjField.fromJson = function (json) {
-        return new ObjField(json.id,
+
+        var field =  new ObjField(json.id,
                             json.title,
                             json.type,
                             Statics.getValueFromString(json.value, json.type))
+
+        field.settings = ObjFieldSettings.fromJson(json.settings)
+
+        return field
     }
 
     ObjField.toJson = function (field) {
         // Return field JSON
         return {
-            id:     field.id,
-            title:  field.title,
-            type:   field.type,
-            value:  Statics.getStringFromValue(field.value, field.type)
-        }
+            id:       field.id,
+            title:    field.title,
+            type:     field.type,
+            value:    Statics.getStringFromValue(field.value, field.type),
+            settings: ObjFieldSettings.toJson(field.settings)
+    }
     }
 
     return ObjField
