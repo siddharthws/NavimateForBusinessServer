c4e8d6ca97387c950e8e2d22b878d23ee4c89821
siddharthg <siddharthg.official@gmail.com>
2018-05-04 19:12:34 +0530

    [APP][UPDATE] Add logic to tell app about mandatory / optional updates

--- grails-app/controllers/navimateforbusiness/api/AppApiController.groovy
+++ grails-app/controllers/navimateforbusiness/api/AppApiController.groovy
@@ -4,7 +4,8 @@ import grails.converters.JSON
 import navimateforbusiness.Acra
 
 class AppApiController {
-    static final int MIN_APP_VERSION_CODE = 23
+    static final int MIN_APP_VERSION_CODE       = 23
+    static final int CURRENT_APP_VERSION_CODE   = 23
 
     def checkForUpdate() {
         int appVersionCode = request.JSON.versionCode
@@ -12,7 +13,11 @@ class AppApiController {
             appVersionCode = 0
         }
 
-        def resp = [updateRequired: (appVersionCode < MIN_APP_VERSION_CODE)]
+        boolean bMandatoryUpdate = (appVersionCode < MIN_APP_VERSION_CODE)
+        boolean bOptionalUpdate = (appVersionCode < CURRENT_APP_VERSION_CODE)
+
+        def resp = [updateRequired: bOptionalUpdate || bMandatoryUpdate,
+                    mandatoryUpdate: bMandatoryUpdate]
         render resp as JSON
     }
 
