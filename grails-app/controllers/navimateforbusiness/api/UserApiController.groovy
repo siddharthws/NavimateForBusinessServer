d64e909c158a0da37a5fd0a5c495b0d69c385eea
siddharthg <siddharthg.official@gmail.com>
2018-05-31 16:36:25 +0530

    [LOCATION][REPORT][FORMS] Send and display forms data in location report - Send all forms submited by the user on selected date - Display special markers with icon when displaying location report - Show form viewer dialog when marker is clicked - Update green and red colors being used for a better feel

--- grails-app/controllers/navimateforbusiness/api/UserApiController.groovy
+++ grails-app/controllers/navimateforbusiness/api/UserApiController.groovy
@@ -8,6 +8,7 @@ class UserApiController {
 
     def authService
     def reportService
+    def formService
     def userService
 
     def changePassword() {
@@ -41,8 +42,14 @@ class UserApiController {
         // Get Report for this rep
         def report = reportService.getLocationReport(rep, dateOnly)
 
+        // Add forms submitted by rep on this date with valid latlng
+        def forms = formService.getForUser(rep)
+        forms = forms.findAll {it.dateCreated.clearTime() == dateOnly}
+        forms = forms.findAll {it.latitude || it.longitude}
+        def formResp = forms.collect {[id: it.id, lat: it.latitude, lng: it.longitude]}
+
         // Send response
-        def resp = [report: report]
+        def resp = [report: report, forms: formResp]
         render resp as JSON
     }
 }
