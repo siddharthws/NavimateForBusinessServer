957dfe59a12de0021cd9b4c04f6ce292442b2743
siddharthg <siddharthg.official@gmail.com>
2018-06-25 16:57:57 +0530

    [FILE][SUPPORT][BACKEND] Add support to upload / downlaod file in backend - Update backend to send any file format when downloading a file - Add new APi to upload file with arbitrary formats

--- grails-app/controllers/navimateforbusiness/api/PhotoApiController.groovy
+++ grails-app/controllers/navimateforbusiness/api/PhotoApiController.groovy
@@ -34,8 +34,8 @@ class PhotoApiController {
         }
 
         // Set response parameters
-        response.setHeader("Content-disposition", "attachment; filename=photo.jpg")
-        response.contentType = grailsApplication.config.getProperty("grails.mime.types.jpg")
+        response.setHeader("Content-disposition", "attachment; filename=" + filename)
+        response.contentType = "application/octet-stream"
 
         // Send file as response
         imageFile.withInputStream { response.outputStream << it }
@@ -69,6 +69,40 @@ class PhotoApiController {
         ObjectMetadata meta = new ObjectMetadata()
         meta.setContentLength(ba.length)
         meta.setContentType("image/jpg")
+        amazonS3Service.storeInputStream(filename, bis, meta)
+
+        // Send filename in response
+        def resp=  [success: true]
+        render resp as JSON
+    }
+
+    // APi to upload photo form app
+    def uploadFile() {
+        // Check app's ID
+        def id = request.getHeader("id")
+        User rep = User.findById(id)
+        if (!rep) {
+            throw new ApiException("Unauthorized", Constants.HttpCodes.UNAUTHORIZED)
+        }
+
+        // Convert multipart data to byte array
+        def multipartData = params.uploadedFile
+        String filename = multipartData.originalFilename
+        InputStream is = multipartData.getInputStream()
+        ByteArrayOutputStream baos = new ByteArrayOutputStream()
+        byte[] buffer = new byte[1024]
+        int bytesRead = is.read(buffer)
+        while (bytesRead > 0) {
+            baos.write(buffer, 0, bytesRead)
+            bytesRead = is.read(buffer)
+        }
+        def ba = baos.toByteArray()
+
+        // Upload to S3 with a UUID filename
+        ByteArrayInputStream bis = new ByteArrayInputStream(ba)
+        ObjectMetadata meta = new ObjectMetadata()
+        meta.setContentLength(ba.length)
+        meta.setContentType("*/*")
         amazonS3Service.storeInputStream(filename, bis, meta)
 
         // Send filename in response
