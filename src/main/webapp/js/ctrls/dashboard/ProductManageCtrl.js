3ebae334f406d2be593d7b419f1536ac2857038b
aroha_blue <amit42308@gmail.com>
2018-07-15 00:00:47 +0530

    [PRODUCT][REMOVE] Added remove support for Products.

--- src/main/webapp/js/ctrls/dashboard/ProductManageCtrl.js
+++ src/main/webapp/js/ctrls/dashboard/ProductManageCtrl.js
@@ -2,7 +2,8 @@
  * Created by aroha on 06-06-2018.
  */
 
-app.controller("ProductManageCtrl", function ( $scope, $rootScope, NavService, DialogService, TableService, ProductService) {
+app.controller("ProductManageCtrl", function ( $scope, $http, $rootScope, $localStorage,  ToastService,
+                                               NavService, DialogService, TableService, ProductService) {
         /* ------------------------------- INIT -----------------------------------*/
         var vm = this
 
@@ -36,6 +37,43 @@ app.controller("ProductManageCtrl", function ( $scope, $rootScope, NavService, D
         vm.sync = function () {
             // Broadcast Toggle Columns Event
             $scope.$broadcast(Constants.Events.TABLE_SYNC)
+        }
+
+        // APIs for actions in dropdown
+        vm.remove = function() {
+            // Launch Confirm Dialog
+            DialogService.confirm(
+                "Are you sure you want to remove " + vm.table.selection.length + " products ?",
+                function () {
+                    $rootScope.showWaitingDialog("Please wait while we are removing products...")
+                    // Make Http call to remove products
+                    $http({
+                        method: 'POST',
+                        url: '/api/manager/products/remove',
+                        headers: {
+                            'X-Auth-Token': $localStorage.accessToken
+                        },
+                        data: {
+                            ids: vm.table.getSelectedIds()
+                        }
+                    }).then(
+                        function (response) {
+                            $rootScope.hideWaitingDialog()
+
+                            // Reset table Selection
+                            vm.table.selection = []
+
+                            // Sync data again
+                            vm.sync()
+
+                            // Show Toast
+                            ToastService.toast("products removed...")
+                        },
+                        function (error) {
+                            $rootScope.hideWaitingDialog()
+                            ToastService.toast("Failed to remove products!!!")
+                        })
+                })
         }
 
         /* ------------------------------- Local APIs -----------------------------------*/
