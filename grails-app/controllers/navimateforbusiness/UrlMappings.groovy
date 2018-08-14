9ad5f5ecacc0178a9769bbff38f5c6d4a713e626
aroha_blue <amit42308@gmail.com>
2018-08-14 15:33:17 +0530

    [COMPANY][PHOTO] Added support to upload photo. - Store photo name with account - upload the photo on the amazon server - frontend support to upload photo - Backend Api to upload photo - Backend Api to upload photo

--- grails-app/controllers/navimateforbusiness/UrlMappings.groovy
+++ grails-app/controllers/navimateforbusiness/UrlMappings.groovy
@@ -149,6 +149,7 @@ class UrlMappings {
         "/api/photos/upload"        (controller: "PhotoApi") {action = [POST: "upload"]}
         "/api/photos/uploadFile"    (controller: "PhotoApi") {action = [POST: "uploadFile"]}
         "/api/photos/get"           (controller: "PhotoApi") {action = [GET: "get"]}
+        "/api/photos/uploadCompanyIcon"   (controller: "PhotoApi") {action = [POST: "uploadCompanyIcon"]}
 
         "500"(controller: "Utils", action: "handleError")
         "404"(controller: "Utils", action: "handle404")
